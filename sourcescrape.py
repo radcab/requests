18ce34be43294115dea0149cf4b28818d329b8e9
radcab <123170936+radcab@users.noreply.github.com>
2023-01-28 21:02:29 +0530

    Update sourcescrape.py

--- sourcescrape.py
+++ sourcescrape.py
@@ -3,8 +3,9 @@ import time
 from os import system
 
 #Give it a terminal title
-system("title " + "sourcescrape.py")
+system("title " + "sourcescrape.py / github.com/radcab")
 
+#Make the cool type text function
 def typeStr(str, delay):
     for i in str:
         print(i, end="", flush=True)
@@ -28,6 +29,7 @@ hr1 = "--------------------------------------------------------------"
 
 typeStr("\n"+hr1, 0.02)
 
+#Steal da code!!1!!1
 req = requests.get(webpage)
 
 print("\n")
@@ -35,16 +37,19 @@ typeStr('<<< Would you like to save or print the output? S/P >>> :', delay=0.02)
 print("\n")
 var = input("")
 
+#Make it so their input is always capitalized via the capitalize() function
 if var.capitalize() == "P":
     print(f"\n"+req.text)
     input(f"\n<<< Press Enter to exit the program. >>>")
     
+#Save it to a txt file if they selected S
 elif var.capitalize() == "S":
     with open ('output.txt', 'w') as file:  
         file.write(req.text)
     typeStr("\n"+hr1, 0.02)
     input(f"\n<<< Press Enter to exit the program. >>>") 
     
+#Exit the code if they typed something else
 else:
     print(f"\nInvalid choice!, Qutting in 5s.")
     time.sleep(5)
