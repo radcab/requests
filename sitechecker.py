007e2aae4bc0b981ac108afbe9df54a2d6cabd00
radcab <123170936+radcab@users.noreply.github.com>
2023-01-28 17:11:05 +0530

    Added Comments

--- sitechecker.py
+++ sitechecker.py
@@ -2,17 +2,22 @@ import requests
 import time
 from os import system
 
+#Make it look cooler by giving it a terminal title :)
 system("title " + "sitechecker.py")
 
 def checkWebsite(website):
     try:
+        #Ctrl + C triggers keyboard interrupt so we will use that to exit our code easily
         print("Press CTRL + C to exit the site checker.")
         while True:
+            #Get the status of the website
             status = requests.get(website)
             code = status.status_code
             time.sleep(3)
+            #Print it out
             print(f"Returned code {code}")
-        
+            
+            #Self explanatory basically
             if code == 200:
                 print("Site is up!")
         
@@ -34,6 +39,7 @@ print('''
 --------------------------------------------------------------
       ''')
 web = input("Input the website you want to check: ")
+#Put this just to make it look a little neater
 print("Preparing!")
 
 checkWebsite(web)
