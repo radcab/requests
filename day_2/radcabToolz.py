1c4e1cdebef99c5b352d9afb04db525b5321940c
radcab <123170936+radcab@users.noreply.github.com>
2023-01-30 15:39:26 +0530

    Added a new function, coolTitle().

--- day_2/radcabToolz.py
+++ day_2/radcabToolz.py
@@ -1,14 +1,20 @@
 import time
+import os
+from os import system
 
 version = 1.0
 author = "github.com/radcab"
 
 #just some functions to make my life a whole lot easier
+def coolTitle(title):
+    system("title " + title)
+
 def readFile(file):
     with open(file) as f:
         lines = f.readlines()
-        return lines
-    
+        new_lst = [x[:-1] for x in lines]
+        return new_lst
+
 def typeStr(str, delay):
     for i in str:
         print(i, end="", flush=True)
